2223d5f7468c3f8c051ca91a09be93b53c754e73
ndreey <72344166+ndreey@users.noreply.github.com>
2026-03-25 17:13:37 +0100

    fixed slow terminal on hpc due to conda init and sourcing bashrc twice

--- install/mousterian-mamba.sh
+++ install/mousterian-mamba.sh
@@ -4,16 +4,38 @@
 source install/mamba/miniforge3.sh
 
 sleep 1
-# Reload bashrc so mamba is now in PATH for this session
 source $HOME/.bashrc
 sleep 1
 
-# Initialize mamba shell integration
 eval "$(mamba shell hook --shell bash)"
 sleep 1
 
-# Set up mamba channels and initialize
 source install/mamba/set-config.sh
 
-# NOW run mamba env installers (they can use mamba create)
+# Overwrite .bashrc with correct paths for lazy loading of conda/mamba.
+# This speeds up shell startup time.
+cat > $HOME/.bashrc << EOF
+#!/bin/bash
+
+[ -f ~/.shell.d/rc ] && . ~/.shell.d/rc
+
+# >>> mamba lazy load >>>
+export MAMBA_EXE='${MAMBA_EXE}'
+export MAMBA_ROOT_PREFIX='${MAMBA_ROOT_PREFIX}'
+export PATH="\$MAMBA_ROOT_PREFIX/bin:\$PATH"
+
+function mamba() {
+    unset -f mamba
+    __mamba_setup="\$("\$MAMBA_EXE" shell hook --shell bash --root-prefix "\$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+    if [ \$? -eq 0 ]; then
+        eval "\$__mamba_setup"
+    else
+        alias mamba="\$MAMBA_EXE"
+    fi
+    unset __mamba_setup
+    mamba "\$@"
+}
+# <<< mamba lazy load <
+EOF
+
 for installer in install/mamba/env-*.sh; do source $installer; done
